de67a0d78430f6756f40ef7309c7e52a039cbaab
decode6 <amitkumar696945@gmail.com>
2024-04-05 10:11:23 +0530

    middleware

--- index.js
+++ index.js
@@ -7,15 +7,30 @@ app.get("/products",(req, res )=>{
     res.send({message: "Get request Successfull"})
 })
 
+//id variable
+
 app.get("/users/:id",(req, res )=>{
 console.log(req.params.id);
 res.send({message: "user response"})
 
 })
 
+app.get("/testing/:id",middleman,(req, res)=>{
+    res.send({message: "testing request "})
+    console.log("next");
+})
 
+//middleware function 
 
-
+function middleman (req, res, next) {
+if(req.params.id<10)
+{
+    res.status(401).send({message: "You are blocked"})
+}
+else{
+    next()
+}
+}
 
 
 
