094722f352ba550f29e9dff6435bbc557c389998
decode6 <amitkumar696945@gmail.com>
2024-04-05 10:51:59 +0530

    post api created

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
+app.use(express.json());
 // app.use(middleman);
 
+app.get("/products", (req, res) => {
+  console.log("get request incoming");
+  res.send({ message: "Get request Successfull" });
+});
 
-app.get("/products",(req, res )=>{
-    console.log("get request incoming");
-    res.send({message: "Get request Successfull"})
-})
+//post request
 
-//post request 
-
-app.post("/products",(req, res )=>{
-
-    console.log(req.params.id);
-    res.send({message: "post working"});
-})
-
+app.post("/products", (req, res) => {
+  console.log(req.body)
+  res.send({ message: "post working" });
+});
 
 //id variable
 
-app.get("/users/:id",(req, res )=>{
-console.log(req.params.id);
-res.send({message: "user response"})
+app.get("/users/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send({ message: "user response" });
+});
 
-})
+app.get("/testing/:id", middleman, (req, res) => {
+  res.send({ message: "testing request " });
+  console.log("next");
+});
 
-app.get("/testing/:id",middleman,(req, res)=>{
-    res.send({message: "testing request "})
-    console.log("next");
-})
+//middleware function
 
-//middleware function 
-
-function middleman (req, res, next) {
-if(req.params.id<10)
-{
-    res.status(401).send({message: "You are blocked"})
-}
-else{
-    next()
-}
+function middleman(req, res, next) {
+  if (req.params.id < 10) {
+    res.status(401).send({ message: "You are blocked" });
+  } else {
+    next();
+  }
 }
 
-
-
-app.listen(8080,()=>{
-    console.log("server running ");
-})
+app.listen(8080, () => {
+  console.log("server running ");
+});
